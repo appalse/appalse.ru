19d8fac68dc7fd7e29245233121c62840de15838
appalse <natalia.kutina13@gmail.com>
2020-01-16 11:04:48 +0300

    add non uglified script

--- app/static/design-system-script.js
+++ app/static/design-system-script.js
@@ -1,1 +1,47 @@
-"use strict";function changeTheme(o){o.classList.toggle("onoffswitch_checked");const e=document.getElementsByClassName("theme");for(let o of e)(o.classList.contains("theme_color_project-inverse")||o.classList.contains("theme_color_project-default"))&&(o.classList.toggle("theme_color_project-inverse"),o.classList.toggle("theme_color_project-default"))}function toggleAccordion(o){let e=o.childNodes;for(let o of e)o.classList.contains("e-accordion__more")&&(o.style.display=o.style.display?"":"block")}const bodyElement=document.body;console.log("bodyELement:"),console.log(bodyElement),bodyElement&&bodyElement.addEventListener("click",(function(o){console.log("event:"),console.log(o),console.log("event.target:"),console.log(o.target);const e=o.target.closest(".onoffswitch"),t=o.target.closest(".e-accordion__short");console.log("onoffButton:"),console.log(e),console.log("accordionShort:"),console.log(t),e?(console.log("go in onoffButton"),changeTheme(e)):t&&(console.log("go in accordionShort"),toggleAccordion(t.parentNode))}),!0);
+'use strict';
+
+function changeTheme(target) {
+	target.classList.toggle('onoffswitch_checked');
+	const themeElements = document.getElementsByClassName('theme');
+	for (let themeElement of themeElements) {
+		if (themeElement.classList.contains('theme_color_project-inverse') ||
+			themeElement.classList.contains('theme_color_project-default')) {
+				themeElement.classList.toggle('theme_color_project-inverse');
+				themeElement.classList.toggle('theme_color_project-default');
+		}
+	}
+}
+
+function toggleAccordion(accordion) {
+	let children = accordion.childNodes;
+	for (let child of children) {
+		if (child.classList.contains('e-accordion__more')) {
+			child.style.display = child.style.display ? '' : 'block';
+		}
+	}
+}
+
+const bodyElement = document.body;
+console.log('bodyELement:');
+console.log(bodyElement);
+if (bodyElement)  {
+	bodyElement.addEventListener('click', function(event) {
+		console.log('event:');
+		console.log(event);
+		console.log('event.target:');
+		console.log(event.target);
+		const onoffButton = event.target.closest('.onoffswitch');
+		const accordionShort = event.target.closest('.e-accordion__short');
+		console.log('onoffButton:');
+		console.log(onoffButton);
+		console.log('accordionShort:');
+		console.log(accordionShort);
+		if (onoffButton) {
+			console.log('go in onoffButton');
+			changeTheme(onoffButton);
+		} else if (accordionShort) {
+			console.log('go in accordionShort');
+			toggleAccordion(accordionShort.parentNode);
+		}
+	}, true);
+}
