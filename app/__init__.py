bd7b790e6a5b66b1356752f7a8c9726746da4172
appalse <natalia.kutina13@gmail.com>
2020-02-11 16:16:55 +0300

    add sportmaster routes

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,10 @@ def designSystem():
 def designSystemProduct():
 	return render_template('design-system-product.html');
 
+@app.route("/sportmaster/")
+def sportmaster():
+	return render_template('sportmaster.html');
+
 @app.errorhandler(404)
 def not_found(error):
 	return render_template('404.html'), 404
